22b08a2bc803332ea7a52db0f73ac499432521c9
alimisumanth <alimisumanth@gmail.com>
2018-12-06 19:05:33 +0530

    adding files

---
+++ IOT/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+
+    url(r'^$', views.homepage, name='index'),
+    url(r'^workshops/$', views.workshops, name='workshops'),
+    url(r'^course/$', views.course, name='course'),
+    url(r'^achievements/$', views.achievements, name='achievements'),
+    url(r'^corebody/$', views.corebody, name='corebody'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^projects/$', views.projects, name='Projects'),
+    url(r'^photo_gallery/$', views.photo_gallery, name='Projects'),
+
+]
+urlpatterns += staticfiles_urlpatterns()
